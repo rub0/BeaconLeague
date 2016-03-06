0c9bdf77c5655374032f8b92ece0e49eca66b025
Thenanox <thenanox@gmail.com>
2016-03-06 18:47:29 +0100

    fix(beaconCollision): now beacon collision is detected in creation and beacon update

--- Source/BeaconLeague/BeaconLeagueGameMode.cpp
+++ Source/BeaconLeague/BeaconLeagueGameMode.cpp
@@ -20,32 +20,24 @@ void ABeaconLeagueGameMode::SpawnBeacon(const FVector &position)
 {
 	FVector respos = (position + FVector(0, 0, 20.0f));
 
-	if (BeaconsSpawned.Num() == MAX_BEACONS)
+	TArray<FOverlapResult> overlapResult;
+
+	FCollisionQueryParams oParams(false);
+	oParams.AddIgnoredActor(this);
+
+	FCollisionResponseParams ResponseParam(ECollisionResponse::ECR_Block);
+
+	World->OverlapMulti(overlapResult, respos, FQuat::Identity, ECollisionChannel::ECC_Pawn, FCollisionShape::MakeSphere(50), oParams);
+
+	if (overlapResult.Num() > 1)
 	{
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("no puedes spawnear el beacon ahi! %d "), overlapResult.Num()));
 
-		TArray<FOverlapResult> overlapResult;
-
-		FCollisionQueryParams oParams(false);
-		oParams.AddIgnoredActor(this);
-
-		FCollisionResponseParams ResponseParam(ECollisionResponse::ECR_Block);
-
-		World->OverlapMulti(overlapResult, respos, FQuat::Identity, ECollisionChannel::ECC_Pawn, FCollisionShape::MakeSphere(50), oParams);
-		
-		if (overlapResult.Num() > 1)
-		{
-			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("no puedes spawnear el beacon ahi! %d "), overlapResult.Num()));
-			
-		}
-		else
-		{
-			BeaconsSpawned[NnewBeacon]->SetActorLocation(respos);
-			NnewBeacon = (NnewBeacon + 1) % MAX_BEACONS;
-		}
-		
-
-
-		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("NnewBeacon ~> %d"), NnewBeacon));
+	}
+	else if (BeaconsSpawned.Num() == MAX_BEACONS)
+	{
+		BeaconsSpawned[NnewBeacon]->SetActorLocation(respos);
+		NnewBeacon = (NnewBeacon + 1) % MAX_BEACONS;
 	}
 	else
 	{
@@ -60,5 +52,4 @@ void ABeaconLeagueGameMode::SpawnBeacon(const FVector &position)
 			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString::Printf(TEXT("you cannot spawn a beacon there!")));
 		}
 	}
-	
 }
